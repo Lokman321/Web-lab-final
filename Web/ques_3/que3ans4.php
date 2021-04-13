414a6937224ba90e610fa66a3bc0d8a33c88b633
Lokman321 <82448243+Lokman321@users.noreply.github.com>
2021-04-13 22:25:17 +0600

    Add files via upload

---
+++ Web/ques_3/que3ans4.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>College</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">College</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#teacher">Teacher Info</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#notice">Notice Board</a>
+        </li>
+        
+
+      </ul>
+    </div>
+  </div>
+</nav>
+<div id="teacher">
+    <div class="ban_desc">
+     <h2>Study For Best Future</h2>
+     <h6>We Prove Best teaching</h6>
+     <h6>Stay with Best hostel service</h6>
+     <button class="btn btn-danger">Start Now</button>
+    </div>
+    <div class="ban_image">
+     <img src="1.jpg" alt="">
+    </div>
+</div>
+<div class="clear_div">
+
+</div>
+<div class="teacher">
+<br>
+<br>
+<h1 class="text-center">Teacher Info</h1>
+<br>
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Mr. John</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-danger">Details</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Mr. John</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-danger">Details</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Mr. John</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-danger">Details</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+<div id="notice" class="container">
+    <br>
+    <br>
+    <h1 class="text-center">Notice Board</h1>
+    <br>
+    <br>
+        <div class="row">
+            <div class="col-sm">
+            <div class="card" style="width: 18rem;">
+        <img src="3.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Off Day</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-danger">See full Notice</a>
+        </div>
+        </div>
+            </div>
+            <div class="col-sm">
+            <div class="card" style="width: 18rem;">
+        <img src="3.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Corona</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-danger">See full Notice</a>
+        </div>
+        </div>
+            </div>
+            <div class="col-sm">
+            <div class="card" style="width: 18rem;">
+        <img src="3.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">College Open</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-danger">See full Notice</a>
+        </div>
+        </div>
+            </div>
+        </div>
+        </div>
+
+</div>
+
+
+<div id="about" class="container">
+    <br>
+    <br>
+
+<h1 class="text-center">
+    About Us
+</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore asperiores sunt temporibus quod fugit distinctio neque vitae adipisci sed nostrum laboriosam animi, sapiente ea eligendi! Magnam, possimus modi nemo est impedit eligendi eos suscipit, aspernatur at, nesciunt alias enim laudantium.</p>
+</div>
+<br>
+<br>
+<p class="foot">Template Made By Naim Istiak Masum</p>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
